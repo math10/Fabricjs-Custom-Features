2dfc8a99602838b679015d81a97422aa329e1960
Md. Musfiqur Rahman Sanim <sanim@ad.enosisbd.com>
2017-06-05 10:42:21 +0600

    Added download option

---
+++ src/script/download.js
@@ -0,0 +1,12 @@
+function downloadCanvas(link, canvasId, filename) {
+    link.href = document.getElementById(canvasId).toDataURL();
+    link.download = filename;
+}
+
+/** 
+ * The event handler for the link's onclick event. We give THIS as a
+ * parameter (=the link element), ID of the canvas and a filename.
+*/
+document.getElementById('download').addEventListener('click', function() {
+    downloadCanvas(this, 'canvas', 'test.png');
+}, false);
